382d99f409827cc53b0dc33498dc0318da081fac
AAHollis <87452103+AAHollis@users.noreply.github.com>
2021-09-20 10:24:50 +1000

    API data showing for temperature, city, wind and humidity

--- src/app.js
+++ src/app.js
@@ -1,7 +1,20 @@
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
+  console.log(response.data);
+
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "cf3b4232190c2a97ceb7d113e0a89390";
-let apiUrl = `api.openweathermap.org/data/2.5/weather?q=New York&appid={$apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+
 axios.get(apiUrl).then(displayTemperature);
